d9c4a4b3afcfae4c7d05803a38836fe6de89aa5b
Sam Hyland <Sam.Hyland@infigenenergy.com>
2019-06-28 11:05:30 +1000

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cpi_escalation_aus-tzam",
+    name="cpi_escalation_aus",
     version="0.0.1",
     author="Samuel Hyland",
     author_email="sam.copycat@gmail.com",
